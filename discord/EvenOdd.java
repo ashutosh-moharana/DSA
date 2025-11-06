7eb6402e0fc8b5675b112f461fa6970bfabd1186
Ashutosh Moharana <ashutoshmoharana00@gmail.com>
2025-11-06 23:30:11 +0530

    Add EvenOdd package

---
+++ discord/EvenOdd.java
@@ -0,0 +1,31 @@
+package discord;
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int no = sc.nextInt();
+
+        if (!isOdd(no)) { // you can use !isOdd() 
+            System.out.println("It is an Even number");
+        } else {
+            System.out.println("It is an Odd number");
+        }
+
+        sc.close();
+
+    }
+
+    public static boolean evenOdd(int n) {
+        return n % 2 == 0;
+    }
+
+    public static boolean isOdd(int n) {
+        return (n >> 1) == 1; //this part I learned from Kunal Kushwaha...
+        /* It simply means the no first converted to boolean and then the right most bit is extracted.If it is one then it is an Odd number and if it is 0 then 
+        it is an Even number.
+         */
+    }
+
+}
