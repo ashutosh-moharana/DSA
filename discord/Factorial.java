c7b56c91a21de8501bc9a41c0bf9e5d9f7cbfbc6
Ashutosh Moharana <ashutoshmoharana00@gmail.com>
2025-11-09 19:09:46 +0530

    Add Factorial code

---
+++ discord/Factorial.java
@@ -0,0 +1,24 @@
+package discord;
+
+import java.util.*;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
+
+        int fact = fact(no);
+        System.out.println("The factorial of " + no + " is " + fact);
+        sc.close();
+    }
+
+    public static int fact(int n) {
+       //solved this factorial POD using recursion
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
+
+    }
+}
